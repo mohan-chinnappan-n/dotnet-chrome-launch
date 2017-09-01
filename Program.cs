3e30256b8cdccc8e977a4c48699c0b734276c94c
Mohan Chinnappan <mohan.chinnappan.n@gmail.com>
2017-09-01 13:16:18 -0400

    init

--- Program.cs
+++ Program.cs
@@ -1,14 +1,3 @@
-### Demo showing opening Chrome Browser in Ubuntu using DotNet #C code
-
-![chrome dot net](./img/chrome-dotnet-1.gif)
-
-
-
-### code
-
-```c#
-
-
 using System.Diagnostics;
 
 namespace hwapp {
@@ -29,15 +18,3 @@ namespace hwapp {
         }
     }
 }
-```
-
-### DotNet version used 
-
-``` 
-$ dotnet --version
-2.0.0
-
-```
-### How to change it to other Operating Systems
-
-- Change the variable chromeExeName to correct value
